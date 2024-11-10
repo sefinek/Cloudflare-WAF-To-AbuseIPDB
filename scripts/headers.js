f843d73604e963f0f27583217c00211de4497e29
Sefinek <contact@sefinek.net>
2024-11-10 01:06:42 +0100

    UserAgent

--- scripts/headers.js
+++ scripts/headers.js
@@ -1,15 +1,15 @@
 const { name, version, homepage } = require('../package.json');
 
-const userAgent = `Mozilla/5.0 (compatible; ${name}/${version}; +${homepage})`;
+const UserAgent = `Mozilla/5.0 (compatible; ${name}/${version}; +${homepage})`;
 
 const CLOUDFLARE = {
-	'User-Agent': userAgent,
+	'User-Agent': UserAgent,
 	'Content-Type': 'application/json',
 	'Authorization': `Bearer ${process.env.CLOUDFLARE_API_KEY}`,
 };
 
 const ABUSEIPDB = {
-	'User-Agent': userAgent,
+	'User-Agent': UserAgent,
 	'Content-Type': 'application/json',
 	'Key': process.env.ABUSEIPDB_API_KEY,
 };
