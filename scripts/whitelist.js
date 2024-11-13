7ead2b9b371c2cd729bbf6ab32e7930abae1c0b7
Sefinek <contact@sefinek.net>
2024-11-13 07:50:36 +0100

    Add `'bucket.'`

--- scripts/whitelist.js
+++ scripts/whitelist.js
@@ -1,4 +1,4 @@
-const subdomains = ['api.', 'cdn.'];
+const subdomains = ['api.', 'cdn.', 'bucket.'];
 const userAgents = ['Chrome/129', 'Chrome/130', 'Chrome/131', 'Chrome/132', 'StellaLauncher', 'PrepareStella'];
 const endpoints = ['/api/', '//video', '//js', '//images', '//imgs', 'favicon.ico', 'sitemap.xml', 'robots.txt'];
 
