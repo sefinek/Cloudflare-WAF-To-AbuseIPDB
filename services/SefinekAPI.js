0106ef028e971ab61e9ea39d93a16bff86ded3c7
Sefinek <contact@sefinek.net>
2025-02-02 03:14:04 +0100

    Update

--- services/SefinekAPI.js
+++ services/SefinekAPI.js
@@ -35,9 +35,10 @@ module.exports = async () => {
 
 		uniqueLogs.forEach(ip => updateSefinekAPIInCSV(ip.rayId, true));
 	} catch (err) {
-		const msg = err.response?.data?.message || err.message;
-		if (!msg?.includes('No valid or unique')) {
-			log(2, `Failed to send logs to Sefinek API! Status: ${err.response?.status ?? 'Unknown'}; Message: ${msg}`);
+		const msg = err?.response?.data?.message;
+		const errMsg = Array.isArray(msg) ? msg[0] : msg || err.message;
+		if (!errMsg?.includes('No valid or unique')) {
+			log(2, `Failed to send logs to Sefinek API! Status: ${err?.response?.status ?? 'Unknown'}; Message: ${errMsg}`);
 		}
 	}
 };
