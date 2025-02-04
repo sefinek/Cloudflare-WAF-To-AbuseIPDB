872cce6207029fd701024dc1e5021a960a285de7
Sefinek <contact@sefinek.net>
2025-02-04 17:12:14 +0100

    Update whitelist.js

--- utils/whitelist.js
+++ utils/whitelist.js
@@ -1,4 +1,26 @@
-const domains = ['blocklist.sefinek.net', 'bucket.sefinek.net'];
-const endpoints = ['favicon.ico', 'favicon.png', 'sitemap.xml', 'robots.txt'];
+const domains = [
+	'api.',
+	'cdn.',
+	'static.',
+	'assets.',
+	'media.',
+	'auth.',
+	'files.',
+	'blocklist.sefinek.net',
+	'bucket.sefinek.net',
+];
+
+const endpoints = [
+	'favicon.ico',
+	'favicon.png',
+	'sitemap.xml',
+	'robots.txt',
+	'ads.txt',
+	'security.txt',
+	'humans.txt',
+	'manifest.json',
+	'apple-touch-icon.png',
+	'crossdomain.xml',
+];
 
 module.exports = { domains, endpoints };
