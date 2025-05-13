788c4a98988511dcc3f67a5af7aa2d963ac6f579
Sefinek <contact@sefinek.net>
2025-05-13 22:42:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //   Copyright 2024-2025 © by Sefinek. All Rights Reserved.
-//                     https://sefinek.net
+//                    https://sefinek.net
 
 const { CronJob } = require('cron');
 const banner = require('./scripts/banners/cloudflare.js');
